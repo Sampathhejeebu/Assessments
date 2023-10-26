b903e2dbafc2276f59cc7797331d7c1c2922356d
Sampathhejeebu <144875933+Sampathhejeebu@users.noreply.github.com>
2023-10-26 17:42:13 +0530

    Problem-1

---
+++ Week6/Program.cs
@@ -0,0 +1,22 @@
+using OnlineInventoryManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineInventoryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+           //DataLayer.InsertProductCategory(5, "Fiidge", 30000, 1);
+           // DataLayer.RetrieveProductDetails();
+           // DataLayer.UpdateProductDetails(4, "Fridge", 45000, 1);
+           // DataLayer.deleteprod(4);
+
+        }
+    }
+}
